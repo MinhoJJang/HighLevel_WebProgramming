267ee50c9a22af9c4bc1920a86e79697ae78b516
MinhoJJang <ahchjang@naver.com>
2023-03-15 21:00:19 +0900

    0315

--- Assignment/Week1/lecture01_homework/src/App.js
+++ Assignment/Week1/lecture01_homework/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import Carinfo from "./pages/Carinfo";
+import Userinfo from "./pages/Userinfo";
+import NoPage from "./pages/NoPage";
+import Layout from "./pages/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="carinfo" element={<Carinfo />} />
+          <Route path="userinfo" element={<Userinfo />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Assignment/Week1/lecture01_homework/src/pages/Carinfo.js
@@ -0,0 +1,12 @@
+import ComponentName from "../Car";
+
+const Carinfo = () => {
+  return (
+    <div>
+      <h1>Car Information</h1>
+      <ComponentName />
+    </div>
+  );
+};
+
+export default Carinfo;
