8594c5ddc24d6ed35e4a1e98215d70d66692fa4a
MinhoJJang <ahchjang@naver.com>
2023-03-20 15:33:19 +0900

    0320

--- Assignment/Week2/lecture02_in_class_exercises/reacthook/src/Calculator.js
+++ Assignment/Week2/lecture02_in_class_exercises/reacthook/src/Calculator.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import "./styles.css";
+//import "./styles.css";
 
 export default function Calculator() {
-  const [num1, setNum1] = useState(0);
-  const [num2, setNum2] = useState(0);
-  const [result, setResult] = useState(0);
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [add, setAdd] = useState("");
+  const [sub, setSub] = useState("");
+  const [mul, setMul] = useState("");
+  const [div, setDiv] = useState("");
 
   const handleNum1Change = (event) => {
     setNum1(Number(event.target.value));
@@ -14,37 +17,50 @@ export default function Calculator() {
     setNum2(Number(event.target.value));
   };
 
-  const handleAdd = () => {
-    setResult(num1 + num2);
+  const addHandler = () => {
+    setAdd(num1 + num2);
   };
 
-  const handleSubtract = () => {
-    setResult(num1 - num2);
+  const subHandler = () => {
+    setSub(num1 - num2);
   };
 
-  const handleMultiply = () => {
-    setResult(num1 * num2);
+  const mulHandler = () => {
+    setMul(num1 * num2);
   };
 
-  const handleDivide = () => {
-    setResult(num1 / num2);
+  const divHandler = () => {
+    setDiv(num1 / num2);
   };
 
   return (
-    <div className="App" style={{ textAlign: "center" }}>
+    <div align="center">
       <h1>GCU Calculator</h1>
       <div>
-        <input type="number" value={num1} onChange={handleNum1Change} />
-        <input type="number" value={num2} onChange={handleNum2Change} />
+        <input type="text" value={num1} onChange={handleNum1Change} />
+        <input type="text" value={num2} onChange={handleNum2Change} />
       </div>
       <div>
-        <button onClick={handleAdd}>Add</button>
-        <button onClick={handleSubtract}>Sub</button>
-        <button onClick={handleMultiply}>Mul</button>
-        <button onClick={handleDivide}>Div</button>
-      </div>
-      <div>
-        <p>{result}</p>
+        <button style={{ marginLeft: "15px" }} onClick={addHandler}>
+          {" "}
+          Add{" "}
+        </button>{" "}
+        {add}
+        <button style={{ marginLeft: "15px" }} onClick={subHandler}>
+          {" "}
+          Sub{" "}
+        </button>{" "}
+        {sub}
+        <button style={{ marginLeft: "15px" }} onClick={mulHandler}>
+          {" "}
+          Mul{" "}
+        </button>{" "}
+        {mul}
+        <button style={{ marginLeft: "15px" }} onClick={divHandler}>
+          {" "}
+          Div{" "}
+        </button>{" "}
+        {div}
       </div>
     </div>
   );
